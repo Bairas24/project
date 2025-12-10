573f6d93adfa5a1c8cc58f60b58dcbb39f9ba6aa
Bairas <bairasganiev@mail.ru>
2025-12-10 20:01:37 +0500

    feat: trying add auth get

--- app/api/users.py
+++ app/api/users.py
@@ -9,3 +9,7 @@ router = APIRouter()
 @router.post("/auth")
 def create_user(email: str, password: str, db: Session = Depends(get_db)):
     return user_create(db=db, email=email, password=password)
+
+@router.get("/auth")
+def get_user(email: str, password: str, db: Session = Depends(get_db)):
+    return user_get(db=db, email=email, password=password)
