4f641a712e9b0ece7a4461148adf65b0c6191066
Bairas <bairasganiev@mail.ru>
2026-01-05 23:11:04 +0500

    feat: Use security functions for check user password

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
+from app.core.security import hash_password, verify_password
 
 
 # Создание пользователя
 def user_create(db: Session, email: str, password: str) -> User:
-    new_user = User(email=email, password=password)
+    hashed_password = hash_password(password)
+    new_user = User(email=email, password=hashed_password)
 
     db.add(new_user)
     db.commit()
@@ -13,6 +15,10 @@ def user_create(db: Session, email: str, password: str) -> User:
     return new_user
 
 # Получение пользователя
-def user_get(email: str, password: str, db: Session) -> User:
+def user_get(email: str, password: str, db: Session) -> User | None:
     user = db.query(User).where(User.email == email, User.password == password).first()
-    return user
+
+    if user and verify_password(plain_password=password, hashed_password=user.password):
+        return user
+    else:
+        return None
