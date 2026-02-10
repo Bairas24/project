8cfa7daf8279a6eaf0077bb072ec76327211cc28
Bairas <bairasganiev@mail.ru>
2026-02-10 23:01:13 +0500

    add fkey car_part

--- app/models/car_part.py
+++ app/models/car_part.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.core.db import Base
 
 class CarPart(Base):
     __tablename__ = "car_part"
     id = Column(Integer, primary_key=True)
-    car_id = Column(Integer, nullable=False)
-    part_id = Column(Integer, nullable=False)
+    car_id = Column(Integer, ForeignKey('cars.id'), nullable=False)
+    part_id = Column(Integer,ForeignKey('parts.id'), nullable=False)
