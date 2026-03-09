16984e9436def2c5ea5fdce876e1d9cc23ae1bf4
miocjlutejllo <miocjlutejll@gmail.com>
2026-03-09 23:06:31 +0500

    Created get part names join route

--- app/api/car.py
+++ app/api/car.py
@@ -1,6 +1,6 @@
 from app.core.db import get_db
 from fastapi import APIRouter, Depends, HTTPException
-from app.services.parts import get_parts
+from app.services.parts import get_parts, get_parts_with_join
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -10,5 +10,11 @@ router = APIRouter()
 @router.get("/")
 def get_cars(brand: str, model: str, db: Session = Depends(get_db)):
     cars = get_parts(db=db, brand = brand, model = model)
+    
+    return cars
+
+@router.get("/with_join")
+def get_cars(brand: str, model: str, db: Session = Depends(get_db)):
+    cars = get_parts_with_join(db=db, brand = brand, model = model)
     
     return cars
--- app/services/parts.py
+++ app/services/parts.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 from app.models.car import Cars
 from app.models.part import Part
 from app.models.car_part import CarPart
@@ -7,38 +8,38 @@ from app.models.car_part import CarPart
 # Входные данные
 
 def get_parts(db: Session, brand: str, model: str):
+    # 1. получаем автомобили по brand и model
     cars = db.query(Cars).where(Cars.brand == brand, Cars.model == model).all()
     
     car_id_list = []
     for car in cars:
         car_id_list.append(car.id)
-        print(car_id_list)
+        
+    # 2. получаем связи авто и запчасти по списку id из пункта 1
     cars_parts = db.query(CarPart).where(CarPart.car_id.in_(car_id_list)).all()
-
-
 
     parts_id = []
     for x in cars_parts:
         parts_id.append(x.part_id)
-    print(parts_id)
    
-    Parts = db.query(Part).where(Part.id.in_(parts_id)).all()
+    # 3. получаем запчатси по списку id из пункта 2
+    parts = db.query(Part).where(Part.id.in_(parts_id)).all()
+
     part_name_list = []
-    for part in Parts:
+    for part in parts:
         part_name_list.append(part.name)
-        print(part_name_list)
+
     return part_name_list
 
+def get_parts_with_join(db: Session, brand: str, model: str):
+    parts = db.query(Part.name).join(CarPart, CarPart.part_id == Part.id).join(Cars, Cars.id == CarPart.car_id).where(Cars.brand == brand, Cars.model == model).all()
 
+    part_name_list = []
+    for part in parts:
+        part_name_list.append(part.name)
 
-    #разобрать код 
-    # вернуть список запчасти Parts
-    # видосы про циклы + массивы 
+    return part_name_list
 
-    # cars_parts  - лист []
-
-    # x -каждый элемент листа cars_parts 
-
-
-
-
+    # stmt = select(Part.name).join(CarPart, CarPart.part_id == Part.id).join(Cars, Cars.id == CarPart.car_id).where(Cars.brand == brand, Cars.model == model)
+    # parts = db.execute(stmt).scalars().all()
+    # return parts
